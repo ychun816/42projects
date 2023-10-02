7c955be287362807c07fe51bb1149e7fe3459923
ychun816 <105208178+ychun816@users.noreply.github.com>
2023-10-02 18:34:27 +0800

    1002 update

--- c/c01/prc.c
+++ c/c01/prc.c
@@ -1,52 +1,39 @@
 
-
-
-void ft_rev_int_tab(int *tab, int size)
-{
-    int left;
-    int right;
+void ft_swap(int *a, int *b)
+{   
     int swap;
 
-    left = 0;
-    right = size - 1;
-    while (left < right)
-    {   
-        swap = tab[left];
-        tab[left] = tab[right];
-        tab[right] = swap;
-        left++;
-        right--;
+    swap = *a;
+    *a = *b;
+    *b = swap;
+}
+
+void ft_sort_int_tab(int *tab, int size)
+{
+    int front_check;
+    int back;
+
+    front_check = 1;
+    while (front_check < (size - 1))
+    {
+        back = front_check + 1;
+        while (back < (size - 1))
+        {
+            if (tab[front_check] > tab[back])
+                ft_swap(&tab[front_check], &tab[back]);
+            back++;
+        }
+        front_check++;
     }
 }
+
 #include <stdio.h>
-// int main()
-// {
-//     int array[5] = {1, 2, 3, 4, 5};
-//     int i;
-//     int size;
-
-//     i = 0;
-//     size = 5;
-//     ft_rev_int_tab(array, size);
-//     while (i < size)
-//     {
-//         printf("rev: %i\n", array[i]);
-//         i++;
-//     }
-//     return 0;
-// }
-#include <stdlib.h>
-int main(int ac, char *av[])
+int main()
 {
-    int size;
-    int i;
-    int test[i] = &av[1];
+    int array[4] = {2, 3, 1, 0};
 
-    size = atoi(av[2]);
-    while (ac == 3)
-    {
-        ft_rev_int_tab(test, size);
-        printf("rev: %i", test[i]);
-        i++;
-    }
+    printf("original: %i, %i, %i, %i\n", array[0], array[1], array[2], array[3]);
+    ft_sort_int_tab(array, 4);
+    printf("sort: %i, %i, %i, %i\n", array[0], array[1], array[2], array[3]);
+    return 0;
 }
