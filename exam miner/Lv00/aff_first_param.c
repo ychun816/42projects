15af6156d94738fd440c117198b932b8af418b9b
ychun816 <105208178+ychun816@users.noreply.github.com>
2023-12-20 17:51:06 +0800

    1220 prc update

---
+++ exam miner/Lv00/aff_first_param.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void put_str(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int main(int ac, char *av[])
+{
+    if (ac >= 2)
+    {
+        put_str(av[1]);
+    }
+    write(1, "\n", 1);
+    return 0;
+}
