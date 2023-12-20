4ab9ddc0b8ffbbd9ae3ec59a8718226fc719149a
ychun816 <105208178+ychun816@users.noreply.github.com>
2023-12-20 17:59:52 +0800

    Delete exam miner/Lv00 directory

--- exam miner/Lv00/maff_alpha.c
+++
@@ -1,8 +0,0 @@
-
-#include <unistd.h>
-
-int main()
-{
-    write(1, "aBcDeFgHiJkLmNoPqRsTuVwXyZ", 27);
-    return 0;
-}
--- exam miner/Lv00/only_a.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-int main()
-{
-    write(1, "a\n", 2);
-    return 0;
-}
